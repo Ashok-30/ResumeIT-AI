5338b8a5aa2e6ef41faba6425c7f0bad7ed972eb
Ashok-30 <mails2ashok99@gmail.com>
2025-06-03 08:55:17 +0100

    Removed wrongly added root-level files. Structure fixed.

--- src/main/java/com/resumeit/resumeit_backend/repository/ResumeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.resumeit.resumeit_backend.repository;
-
-import com.resumeit.resumeit_backend.model.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-}
